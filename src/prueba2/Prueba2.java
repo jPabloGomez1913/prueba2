dacdd1f91f751239a09d368ec763e063485606e2
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-07-07 15:33:49 -0500

    Se agrego una suma

--- src/prueba2/Prueba2.java
+++ src/prueba2/Prueba2.java
@@ -1,12 +1,22 @@
 
 package prueba2;
 
+import java.util.Scanner;
+
 
 public class Prueba2 {
 
   
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Scanner entrada = new Scanner(System.in);
+        int suma,num,num2;
+        System.out.println("Ingrese un numero");
+        num=entrada.nextInt();
+        System.out.println("Ingrese un numero");
+        num2=entrada.nextInt();
+        suma=num+num2;
+        System.out.println("la suma es: "+suma);
     }
     
 }
